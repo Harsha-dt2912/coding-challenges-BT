d1357d729119f1494326ea360ed94e3753f0b67d
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:46:09 +0530

    add comment

--- loops_and_iterations/even_square_series.py
+++ loops_and_iterations/even_square_series.py
@@ -9,14 +9,14 @@ while True:
         print("Invalid input! Enter an integer.")
 
 print("Series:")
-# i = 2
-# while True:
-#     square = i * i
-#     if square > n:
-#         break
-#     print(square, end=" ")
-#     i += 2
-
-for i in range(2, n * 2+1, 2):
+i = 2
+while True:
     square = i * i
+    if square > n:
+        break
     print(square, end=" ")
+    i += 2
+#upto n terms code
+# for i in range(2, n * 2+1, 2):
+#     square = i * i
+#     print(square, end=" ")

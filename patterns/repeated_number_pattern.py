88914f3263f835548ddaad9813146e434963049a
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:59:59 +0530

    program to print repeated_number pattern with N rows

---
+++ patterns/repeated_number_pattern.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n = int(input("Enter number of rows: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+for i in range(1, n + 1):
+    print(str(i) * 5)

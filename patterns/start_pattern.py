3c637dde55d977eabd2916bd0036352a9fefb0fd
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:55:59 +0530

    program to print star pattern with N rows

---
+++ patterns/start_pattern.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n = int(input("Enter number of rows: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+for _ in range(n):
+    print("*****")

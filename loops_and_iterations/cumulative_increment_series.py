7b7a4a315b28dc24946acd4218c1daead1abf4bb
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:27:23 +0530

    Add program to display series 1,2,4,7,11... up to N

---
+++ loops_and_iterations/cumulative_increment_series.py
@@ -0,0 +1,24 @@
+while True:
+    try:
+        n = int(input("Enter value of N: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+print("Series:")
+term = 1
+increment = 1
+
+while term <= n:
+    print(term, end=" ")
+    term += increment
+    increment += 1
+
+
+# for _ in range(n):
+#     print(term, end=" ")
+#     term += increment
+#     increment += 1

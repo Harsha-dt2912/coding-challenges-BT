b68339065ddd8338a055c7731cd6c1296232b627
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 22:31:32 +0530

    add program to print alternating signed square number triangle

---
+++ patterns/squares_triangle.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        n = int(input("Enter number of rows: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+num = 1  # natural number counter
+
+for i in range(1, n + 1):
+    for _ in range(i):
+        square = num * num
+        if num % 2 == 0:
+            square = -square
+        print(square, end=" ")
+        num += 1
+    print()

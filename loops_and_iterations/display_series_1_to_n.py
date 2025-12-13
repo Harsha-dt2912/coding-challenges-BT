ca8ee7420f5c802ef307e2128b0bf8118f82b019
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:01:20 +0530

    Add program to display series from 1 to N

---
+++ loops_and_iterations/display_series_1_to_n.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n = int(input("Enter value of N: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+for i in range(1, n + 1):
+    print(i, end=" ")

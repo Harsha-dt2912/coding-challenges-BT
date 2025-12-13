a09e731077075c102194ebf371891637ff55216c
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 21:17:58 +0530

    add program to display square of even numbers up to N

---
+++ loops_and_iterations/even_square_series.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        n = int(input("Enter value of N: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input! Enter an integer.")
+
+print("Series:")
+# i = 2
+# while True:
+#     square = i * i
+#     if square > n:
+#         break
+#     print(square, end=" ")
+#     i += 2
+
+for i in range(2, n * 2+1, 2):
+    square = i * i
+    print(square, end=" ")

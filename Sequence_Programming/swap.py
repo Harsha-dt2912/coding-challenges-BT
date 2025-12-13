259b107d5395cb810298c3de1bc583f1e935a8e2
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-13 18:10:51 +0530

    swap of two numbers

---
+++ Sequence_Programming/swap.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+
+print(f"\nBefore swapping: a = {a}, b = {b}")
+
+#  swap
+a, b = b, a
+
+print(f"After swapping: a = {a}, b = {b}")

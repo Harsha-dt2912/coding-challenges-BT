a12decac3d91dd9b7df300653c79dce5b9ff3b0b
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-15 00:05:15 +0530

    Applied grand total discount and quantity-based discount

--- Hackathon/Retail_shopping.py
+++ Hackathon/Retail_shopping.py
@@ -2,11 +2,13 @@
 
 items = []
 grand_total = 0
+total_quantity = 0
 
 while True:
     item_code = input("\nEnter Item Code: ")
     description = input("Enter Item Description: ")
     quantity = int(input("Enter Quantity: "))
+    total_quantity += quantity
     price = float(input("Enter Price per Item: "))
 
     item_total = quantity * price
@@ -20,3 +22,22 @@ while True:
         break
 
 print(f"\nGrand Total: ₹{grand_total:.2f}")
+
+
+# Challenge 27: Discounts
+
+discount = 0
+
+if grand_total > 10000:
+    discount = grand_total * 0.10
+    print(f"10% Discount Applied: ₹{discount:.2f}")
+
+grand_total -= discount
+
+if total_quantity > 20:
+    qty_discount = grand_total * 0.05
+    grand_total -= qty_discount
+    print(f"5% Quantity Discount Applied: ₹{qty_discount:.2f}")
+
+print(f"Grand Total after Discounts: ₹{grand_total:.2f}")
+

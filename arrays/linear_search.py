2193d2a438d335bb3c75f762fc8e45d3b8152f99
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-14 12:14:28 +0530

    print statement changes to linear search

--- arrays/linear_search.py
+++ arrays/linear_search.py
@@ -9,9 +9,9 @@ key = int(input("Enter element to search: "))
 found = False
 for i in range(n):
     if arr[i] == key:
-        print(f"Element {key} found at position {i + 1}")
+        print(f"Element found at position {i + 1}")
         found = True
         break
 
 if not found:
-    print(f"Element {key} not found at the array")
+    print("Element not found")

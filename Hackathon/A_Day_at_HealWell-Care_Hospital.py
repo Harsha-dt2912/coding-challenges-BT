81a0930ee66c41b027564f759597c87b3eb8324a
Harsha-dt2912 <harsha.1si22cs065@gmail.com>
2025-12-15 17:22:54 +0530

    service selection process

--- Hackathon/A_Day_at_HealWell-Care_Hospital.py
+++ Hackathon/A_Day_at_HealWell-Care_Hospital.py
@@ -66,10 +66,72 @@ services = [
 
 costs = [500, 300, 800, 1500, 4000, 7000]
 
-# Patient Details (Validated Input)
+# Patient Details
 
 name = get_valid_patient_name()
 age = get_valid_age()
 gender = get_valid_gender()
 contact = get_valid_contact()
+
+
+# service selection
+
+print("\nAvailable Services:")
+for i, service in enumerate(services, start=1):
+    print(f"{i}. {service}")
+
+print("\nSelect services using comma or space (e.g., 1,3 or 1 3)")
+print("Enter Q to quit without selecting any service.")
+
+selected_services = []
+
+while True:
+    user_choice = input("Select service numbers: ").strip()
+
+    # Quit option 
+    if user_choice.lower() == 'q':
+        print("No services selected. Exiting selection.")
+        selected_services = []
+        break
+
+    # Empty input
+    if not user_choice:
+        print("❌ At least one service must be selected or press Q to quit.")
+        continue
+
+    try:
+        
+        user_input = user_choice.replace(",", " ")
+        choices = [int(i.strip()) for i in user_input.split()]
+
+        selected_services = []   # reset before adding
+        for choice in choices:
+            if 1 <= choice <= len(services):
+                selected_services.append(services[choice - 1])
+            else:
+                raise ValueError
+
+        # Remove duplicates
+        selected_services = list(dict.fromkeys(selected_services))
+
+        break
+
+    except ValueError:
+        print("❌ Invalid selection. Please enter valid service numbers.")
+
+# Fetching Costs of Selected Services
+
+selected_costs = []
+
+if not selected_services:
+    print("No services selected. Billing process terminated.")
+    exit()
+
+for service in selected_services:
+    index = services.index(service)
+    selected_costs.append(costs[index])
+
+print("\nSelected Services and Costs:")
+for i in range(len(selected_services)):
+    print(f"{i+1}. {selected_services[i]} - ₹{selected_costs[i]}")
 
